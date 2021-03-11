a16c7377a88320923ad7f5410b440925a0bd23c1
NdukeSam <diamondsam33@gmail.com>
2021-03-11 15:15:15 +0100

    updated roll dice and hold event listeners

--- app.js
+++ app.js
@@ -12,10 +12,81 @@ var scores, roundScore, activePlayer, dice;
 
 scores =  [0, 0];
 roundScore = 0;
-activePlayer = 1;
-dice = Math.floor(Math.random() * 6) + 1;
+activePlayer = 0;
 
-document.querySelector('#current--' + activePlayer).textContent  = dice;
-// document.querySelector('#current--' +activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+
+
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score--0').textContent = '0';
+document.getElementById('score--1').textContent = '0';
+document.getElementById('current--0').textContent = '0';
+document.getElementById('current--1').textContent = '0';
+
+
+document.querySelector('.btn--roll').addEventListener('click', function () {
+    
+    // 1. Random number
+    
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'img/dice-' + dice + '.png'
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current--' + activePlayer).textContent  = roundScore;
+    }else{
+        //Next Player
+        nextPlayer();
+    }
+
+});
+
+
+document.querySelector('.btn--hold').addEventListener('click', function () {
+   //Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    // Update UI
+    document.querySelector('#score--' + activePlayer).textContent = scores[activePlayer];
+
+    //Check if the player won
+    // scores[activePlayer] >= 20 ? document.querySelector('#name--' + activePlayer).textContent = 'Winner! 🏆' : //Next player
+    // nextPlayer();
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name--' + activePlayer).textContent = 'Winner! 🏆';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player--' + activePlayer).classList.add('winner');
+        document.querySelector('.player--' + activePlayer).classList.remove('player--active');
+
+
+    } else{
+        //Next player
+        nextPlayer();
+    }
+    
+
+
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current--0').textContent = '0';
+        document.getElementById('current--1').textContent = '0';
+
+        document.querySelector('.player--0').classList.toggle('player--active');
+        document.querySelector('.player--1').classList.toggle('player--active');
+        document.querySelector('.dice').style.display = 'none';
+
+        // document.querySelector('.player--0').classList.remove('player--active');
+        // document.querySelector('.player--1').classList.add('player--active');
+}
