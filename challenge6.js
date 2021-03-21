8b60fa60affd86b727dd94cdfc8ba0d8c0f006fc
micaiah-effiong <micaiah.effiong@gmail.com>
2021-03-22 00:35:56 +0100

    Added: Bot as player 2, huge code refactor and formatting

--- challenge6.js
+++ challenge6.js
@@ -9,11 +9,54 @@ GAME RULES:
 
 */
 var scores, roundScore, activePlayer, gamePlaying;
-init();
 
 var previousDice;
 
-document.querySelector(".btn--roll").addEventListener("click", function () {
+const gameDice1 = document.querySelector("#dice-1");
+const gameDice2 = document.querySelector("#dice-2");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
+const currentScore0 = document.querySelector("#current--0");
+const currentScore1 = document.querySelector("#current--1");
+
+const rollBtn = document.querySelector(".btn--roll");
+rollBtn.addEventListener("click", roll);
+
+document.querySelector(".btn--hold").addEventListener("click", hold);
+
+const bot = {
+    currentScore: 0,
+
+    isOkToRollAgain(d1, d2) {
+        return ![d1, d2].includes(1);
+    },
+
+    play() {
+        const diceVal = this.roll();
+        if (this.isOkToRollAgain(diceVal[0], diceVal[1])) {
+            this.currentScore += diceVal[0] + diceVal[1];
+            console.log("bot current-score: " + this.currentScore);
+
+            if (this.currentScore > 20) {
+                this.hold();
+                this.currentScore = 0;
+                return;
+            }
+
+            setTimeout(() => {
+                // wait a little, check to roll again
+                this.play();
+            }, 2500);
+        } else {
+            this.currentScore = 0;
+        }
+    },
+};
+
+bot.roll = roll;
+bot.hold = hold;
+
+function roll() {
     if (gamePlaying) {
         // 1. Random number
         var dice1 = Math.floor(Math.random() * 6) + 1;
@@ -21,8 +64,8 @@ document.querySelector(".btn--roll").addEventListener("click", function () {
 
         // 2. Display the result
         displayDice();
-        document.getElementById("dice-1").src = "img/dice-" + dice1 + ".png";
-        document.getElementById("dice-2").src = "img/dice-" + dice2 + ".png";
+        gameDice1.src = "img/dice-" + dice1 + ".png";
+        gameDice2.src = "img/dice-" + dice2 + ".png";
 
         /* if (dice === 6 && previousDice === 6) {
         console.log('twice!');
@@ -30,28 +73,44 @@ document.querySelector(".btn--roll").addEventListener("click", function () {
         scores[activePlayer] = 0;
         document.querySelector('#score--' + activePlayer).textContent  = '0';
         nextPlayer();
-    }
-    */
+        }
+        */
 
         // 3. Update the round score IF the rolled number was NOT a 1
+        console.log("Player " + (activePlayer + 1) + ":");
+        console.log(dice1 + " + " + dice2 + " = " + (dice1 + dice2));
+
+        roundScore += dice1 + dice2;
+        console.log("round-score", roundScore);
+
+        document.querySelector(
+            "#current--" + activePlayer
+        ).textContent = roundScore;
+
+        if (![dice1, dice2].includes(1)) {
+            nextPlayer();
+        }
+
+        /*
         if (dice1 !== 1 && dice2 !== 1) {
             //Add score
             roundScore += dice1 + dice2;
+            console.log("round-score", roundScore);
             document.querySelector(
                 "#current--" + activePlayer
             ).textContent = roundScore;
         } else {
             //Next Player
             nextPlayer();
-        }
+        }*/
 
         // previousDice = dice;
-        console.log(dice1);
-        console.log(dice2);
-    }
-});
 
-document.querySelector(".btn--hold").addEventListener("click", function () {
+        return [dice1, dice2];
+    }
+}
+
+function hold() {
     if (gamePlaying) {
         //Add CURRENT score to GLOBAL score
         scores[activePlayer] += roundScore;
@@ -92,29 +151,36 @@ document.querySelector(".btn--hold").addEventListener("click", function () {
             nextPlayer();
         }
     }
-});
+}
 
 function hideDice() {
-    document.getElementById("dice-1").style.display = "none";
-    document.getElementById("dice-2").style.display = "none";
+    gameDice1.style.display = "none";
+    gameDice2.style.display = "none";
 }
 
 function displayDice() {
-    document.getElementById("dice-1").style.display = "block";
-    document.getElementById("dice-2").style.display = "block";
+    gameDice1.style.display = "block";
+    gameDice2.style.display = "block";
 }
 
 function nextPlayer() {
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    activePlayer = activePlayer === 0 ? 1 : 0;
     roundScore = 0;
 
-    document.getElementById("current--0").textContent = "0";
-    document.getElementById("current--1").textContent = "0";
+    currentScore0.textContent = "0";
+    currentScore1.textContent = "0";
 
-    document.querySelector(".player--0").classList.toggle("player--active");
-    document.querySelector(".player--1").classList.toggle("player--active");
+    player0.classList.toggle("player--active");
+    player1.classList.toggle("player--active");
 
     hideDice();
+
+    if (activePlayer === 1) {
+        roll.disable = true;
+        bot.play();
+    } else {
+        roll.disable = false;
+    }
 
     // document.querySelector('.player--0').classList.remove('player--active');
     // document.querySelector('.player--1').classList.add('player--active');
@@ -134,22 +200,24 @@ function init() {
 
     document.getElementById("score--0").textContent = "0";
     document.getElementById("score--1").textContent = "0";
-    document.getElementById("current--0").textContent = "0";
-    document.getElementById("current--1").textContent = "0";
+    currentScore0.textContent = "0";
+    currentScore1.textContent = "0";
     //Set Player 1
     document.getElementById("name--0").textContent = "Player 1";
     //Set Player 2
     document.getElementById("name--1").textContent = "Player 2";
     //Remove winner classes from both players
-    document.querySelector(".player--0").classList.remove("winner");
-    document.querySelector(".player--1").classList.remove("winner");
+    player0.classList.remove("winner");
+    player1.classList.remove("winner");
     //Remove active classes from both players
-    document.querySelector(".player--0").classList.remove("player--active");
-    document.querySelector(".player--1").classList.remove("player--active");
+    player0.classList.remove("player--active");
+    player1.classList.remove("player--active");
     //Add active class to Player 1
-    document.querySelector(".player--0").classList.add("player--active");
+    player0.classList.add("player--active");
 }
 
 document.querySelector("#input-form").addEventListener("submit", (e) => {
     e.preventDefault();
 });
+
+init();
